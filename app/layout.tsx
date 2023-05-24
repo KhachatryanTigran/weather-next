53bb400878a5c814344c5b25450d69109ea81486
khachatryantigran <tigran9202@gmail.com>
2023-05-24 14:55:42 +0400

    meta changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {" "}
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
